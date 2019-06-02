6a43a6890d0b7f0e3ec425dcca8c4b11896d8c4e
Suhail T S <ctrsuhail@gmail.com>
2019-06-03 00:20:16 +0530

    horizontal change to viewpger

--- app/src/main/java/com/example/myride/adpter/RecyclerViewHorizontalListAdapter.java
+++ app/src/main/java/com/example/myride/adpter/RecyclerViewHorizontalListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.myride.adpter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,86 +16,117 @@ import com.example.myride.model.Resultsetting;
 
 import java.util.List;
 
-public class RecyclerViewHorizontalListAdapter extends RecyclerView.Adapter<RecyclerViewHorizontalListAdapter.GroceryViewHolder>{
-
+public class RecyclerViewHorizontalListAdapter extends PagerAdapter {
 
 
     private List<Resultsetting> horizontalGrocderyList;
     Context context;
+    LayoutInflater inflater;
 
-    public RecyclerViewHorizontalListAdapter(List<Resultsetting> horizontalGrocderyList, Context context){
-        this.horizontalGrocderyList= horizontalGrocderyList;
+    public RecyclerViewHorizontalListAdapter(List<Resultsetting> horizontalGrocderyList, Context context) {
+        this.horizontalGrocderyList = horizontalGrocderyList;
         this.context = context;
+
+
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
     }
 
-    @Override
+
+
+   /* @Override
     public GroceryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //inflate the layout file
         View groceryProductView = LayoutInflater.from(parent.getContext()).inflate(R.layout.resultfullscreenadapter, parent, false);
         GroceryViewHolder gvh = new GroceryViewHolder(groceryProductView);
         return gvh;
+    }*/
+
+    /* @Override
+     public void onBindViewHolder(GroceryViewHolder holder, final int position) {
+
+         holder.drivername.setText(horizontalGrocderyList.get(position).getDrivername());
+         holder.starting.setText(horizontalGrocderyList.get(position).getStarting());
+         holder.ending.setText(horizontalGrocderyList.get(position).getEnding());
+         holder.departuretime.setText(horizontalGrocderyList.get(position).getDeparturetime());
+         holder.arrivaltime.setText(horizontalGrocderyList.get(position).getArrivaltime());
+         holder.totaltime.setText(horizontalGrocderyList.get(position).getTotaltime());
+         holder.carname.setText(horizontalGrocderyList.get(position).getCarname());
+         holder.regno.setText(horizontalGrocderyList.get(position).getRegno());
+         holder.driverrating.setRating(horizontalGrocderyList.get(position).getRating());
+         holder.driverrating.setRating(horizontalGrocderyList.get(position).getAvailableseat());
+
+         holder.profile.setImageBitmap(horizontalGrocderyList.get(position).getProfile());
+         holder.car.setImageBitmap(horizontalGrocderyList.get(position).getCar());
+
+
+ //        holder.imageView.setImageResource(horizontalGrocderyList.get(position).getProductImage());
+ //        holder.txtview.setText(horizontalGrocderyList.get(position).getProductName());
+ //        holder.imageView.setOnClickListener(new View.OnClickListener() {
+ //            @Override
+ //            public void onClick(View v) {
+ //                String productName = horizontalGrocderyList.get(position).getProductName().toString();
+ //                Toast.makeText(context, productName + " is selected", Toast.LENGTH_SHORT).show();
+ //            }
+ //        });
+     }*/
+    ImageView profile, car;
+
+    TextView drivername, starting, ending, departuretime, arrivaltime, totaltime, carname, regno;
+    RatingBar driverrating, availableseat;
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        View groceryProductView = inflater.inflate(R.layout.resultfullscreenadapter, container, false);
+
+        container.addView(groceryProductView);
+        drivername = groceryProductView.findViewById(R.id.drivername);
+        starting = groceryProductView.findViewById(R.id.stating);
+        ending = groceryProductView.findViewById(R.id.ending);
+        departuretime = groceryProductView.findViewById(R.id.departuretime);
+        totaltime = groceryProductView.findViewById(R.id.totaltime);
+        arrivaltime = groceryProductView.findViewById(R.id.arrivaltime);
+        regno = groceryProductView.findViewById(R.id.regno);
+        carname = groceryProductView.findViewById(R.id.carname);
+        profile = groceryProductView.findViewById(R.id.profile);
+        car = groceryProductView.findViewById(R.id.car);
+        driverrating = groceryProductView.findViewById(R.id.ratin);
+        availableseat = groceryProductView.findViewById(R.id.noofseat);
+
+
+        drivername.setText(horizontalGrocderyList.get(position).getDrivername());
+        starting.setText(horizontalGrocderyList.get(position).getStarting());
+        ending.setText(horizontalGrocderyList.get(position).getEnding());
+        departuretime.setText(horizontalGrocderyList.get(position).getDeparturetime());
+        arrivaltime.setText(horizontalGrocderyList.get(position).getArrivaltime());
+        totaltime.setText(horizontalGrocderyList.get(position).getTotaltime());
+        carname.setText(horizontalGrocderyList.get(position).getCarname());
+        regno.setText(horizontalGrocderyList.get(position).getRegno());
+        driverrating.setRating(horizontalGrocderyList.get(position).getRating());
+        driverrating.setRating(horizontalGrocderyList.get(position).getAvailableseat());
+        profile.setImageBitmap(horizontalGrocderyList.get(position).getProfile());
+        car.setImageBitmap(horizontalGrocderyList.get(position).getCar());
+
+
+        return groceryProductView;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
     }
 
     @Override
-    public void onBindViewHolder(GroceryViewHolder holder, final int position) {
-
-        holder.drivername.setText(horizontalGrocderyList.get(position).getDrivername());
-        holder.starting.setText(horizontalGrocderyList.get(position).getStarting());
-        holder.ending.setText(horizontalGrocderyList.get(position).getEnding());
-        holder.departuretime.setText(horizontalGrocderyList.get(position).getDeparturetime());
-        holder.arrivaltime.setText(horizontalGrocderyList.get(position).getArrivaltime());
-        holder.totaltime.setText(horizontalGrocderyList.get(position).getTotaltime());
-        holder.carname.setText(horizontalGrocderyList.get(position).getCarname());
-        holder.regno.setText(horizontalGrocderyList.get(position).getRegno());
-        holder.driverrating.setRating(horizontalGrocderyList.get(position).getRating());
-        holder.driverrating.setRating(horizontalGrocderyList.get(position).getAvailableseat());
-
-        holder.profile.setImageBitmap(horizontalGrocderyList.get(position).getProfile());
-        holder.car.setImageBitmap(horizontalGrocderyList.get(position).getCar());
-
-
-//        holder.imageView.setImageResource(horizontalGrocderyList.get(position).getProductImage());
-//        holder.txtview.setText(horizontalGrocderyList.get(position).getProductName());
-//        holder.imageView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String productName = horizontalGrocderyList.get(position).getProductName().toString();
-//                Toast.makeText(context, productName + " is selected", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-    }
-
-    @Override
-    public int getItemCount() {
+    public int getCount() {
         return horizontalGrocderyList.size();
     }
 
-    public class GroceryViewHolder extends RecyclerView.ViewHolder {
-        ImageView profile,car;
-
-        TextView drivername,starting,ending,departuretime,arrivaltime,totaltime,carname,regno;
-        RatingBar driverrating,availableseat;
-
-        public GroceryViewHolder(View view) {
-            super(view);
-
-            drivername=view.findViewById(R.id.drivername);
-            starting=view.findViewById(R.id.stating);
-            ending=view.findViewById(R.id.ending);
-            departuretime=view.findViewById(R.id.departuretime);
-            totaltime=view.findViewById(R.id.totaltime);
-            arrivaltime=view.findViewById(R.id.arrivaltime);
-            regno=view.findViewById(R.id.regno);
-            carname=view.findViewById(R.id.carname);
-
-            profile=view.findViewById(R.id.profile);
-            car=view.findViewById(R.id.car);
-            driverrating=view.findViewById(R.id.ratin);
-            availableseat=view.findViewById(R.id.noofseat);
-
-
-
-
-        }
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view.equals(o);
     }
+
 }
--- app/src/main/java/com/example/myride/findride/Resultfullscreen.java
+++ app/src/main/java/com/example/myride/findride/Resultfullscreen.java
@@ -1,9 +1,11 @@
 package com.example.myride.findride;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -13,6 +15,7 @@ import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -28,27 +31,38 @@ public class Resultfullscreen extends AppCompatActivity {
     private List<Resultsetting> rideresultlist = new ArrayList<>();
     private RecyclerView resultrecyclerview;
     private RecyclerViewHorizontalListAdapter resultrecycleradapter;
-Button accept,next;
+    Button accept, next;
     LinearLayoutManager horizontalLayoutManager;
-    int srollcount=0;
+    ViewPager viewPager;
+    int srollcount = 0;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.result);
 
+        initComponents();
 
+        populategroceryList();
+        resultrecycleradapter = new RecyclerViewHorizontalListAdapter(rideresultlist, getApplicationContext());
+
+        viewPager.setAdapter(resultrecycleradapter);
+        resultrecycleradapter.notifyDataSetChanged();
+
+
+/*
         resultrecyclerview = findViewById(R.id.idRecyclerViewHorizontalList);
         // add a divider after each item for more clarity
         resultrecyclerview.addItemDecoration(new DividerItemDecoration(Resultfullscreen.this, LinearLayoutManager.HORIZONTAL));
         resultrecycleradapter = new RecyclerViewHorizontalListAdapter(rideresultlist, getApplicationContext());
           horizontalLayoutManager = new LinearLayoutManager(Resultfullscreen.this, LinearLayoutManager.HORIZONTAL, false);
         resultrecyclerview.setLayoutManager(horizontalLayoutManager);
+        resultrecyclerview.addItemDecoration(new DividerItemDecoration(this, 0));
         resultrecyclerview.setAdapter(resultrecycleradapter);
         PagerSnapHelper mSnapHelper = new PagerSnapHelper();
         mSnapHelper.attachToRecyclerView(resultrecyclerview);
-        accept=findViewById(R.id.accept);
-        next=findViewById(R.id.next);
 
        resultrecycleradapter.notifyDataSetChanged();
 
@@ -57,9 +71,6 @@ Button accept,next;
            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
                super.onScrollStateChanged(recyclerView, newState);
 
-                int visibleItemCount = horizontalLayoutManager.getChildCount();
-               int totalItemCount = horizontalLayoutManager.getItemCount();
-               int pastVisibleItems = horizontalLayoutManager.findFirstCompletelyVisibleItemPosition();
 
                if(srollcount>=resultrecycleradapter.getItemCount()-1){
                    // End of the list is here.
@@ -73,12 +84,12 @@ Button accept,next;
                super.onScrolled(recyclerView, dx, dy);
 
            }
-       });
+       });*/
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (horizontalLayoutManager.findLastCompletelyVisibleItemPosition() == (resultrecycleradapter.getItemCount() -1)) {
+            /*    if (horizontalLayoutManager.findLastCompletelyVisibleItemPosition() == (resultrecycleradapter.getItemCount() -1)) {
 
                     resultrecyclerview.smoothScrollToPosition(0);
                 }
@@ -86,40 +97,85 @@ Button accept,next;
 
                 if (horizontalLayoutManager.findLastCompletelyVisibleItemPosition() < (resultrecycleradapter.getItemCount() - 1)) {
                         resultrecyclerview.smoothScrollToPosition(horizontalLayoutManager.findLastCompletelyVisibleItemPosition() + 1);
-                }
+                }*/
+
+                int currentPosition = viewPager.getCurrentItem();
+                if (currentPosition == resultrecycleradapter.getCount() - 1)
+                    viewPager.setCurrentItem(0);
+                else
+                    viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
+
             }
         });
 
 
-        try {
-            accept.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  Resultsetting resultsetting=  rideresultlist.get(resultrecycleradapter.getItemCount()-1);
+
+                startActivity(new Intent(getApplicationContext(), Paymentacivity.class));
+                finish();
 
 
+            }
+        });
+        /*
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+                if (checkDirection) {
+                    if (thresholdOffset > v) {
+                        Log.wtf(TAG, "going left");
+                        int currentPosition = viewPager.getCurrentItem();
+                        if (currentPosition == resultrecycleradapter.getCount() - 1)
+                            viewPager.setCurrentItem(0);
+                    } else {
+                        Log.wtf(TAG, "going right");
 
-                    Resultsetting resultsetting=  rideresultlist.get(resultrecycleradapter.getItemCount()-1);
 
-                    startActivity(new Intent(getApplicationContext(),Paymentacivity.class));
-                    finish();
-
-
+                    }
+                    checkDirection = false;
                 }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+                if (!scrollStarted && i == ViewPager.SCROLL_STATE_DRAGGING) {
+                    scrollStarted = true;
+                    checkDirection = true;
+                } else {
+                    scrollStarted = false;
+                }
+            }
+        });*/
 
 
-        populategroceryList();
+    }
+
+    private static final String TAG = "Resultfullscreen";
+    private static final float thresholdOffset = 0.5f;
+    private boolean scrollStarted, checkDirection;
+
+    private void initComponents() {
+
+        viewPager = findViewById(R.id.result);
+        accept = findViewById(R.id.accept);
+        next = findViewById(R.id.next);
     }
 
     private void populategroceryList() {
         Bitmap myLogo = BitmapFactory.decodeResource(this.getResources(), R.drawable.yu);
         Bitmap myLogo1 = BitmapFactory.decodeResource(this.getResources(), R.drawable.african);
-        Resultsetting resultsetting = new Resultsetting("Driveaname", "Trivandrum", "Mumbai", "10:50am", "09:00pm", "9Hrs", "Merceedes bens", "Kl1056", (float) 5, (float) 5, myLogo1, myLogo);
+        Resultsetting resultsetting = new Resultsetting("Driveaname", "Trivandrum", "Mumbai", "10:50am", "09:00pm", "9Hrs", "Merceedes bens", "Kl1056", (float) 3, (float) 3, myLogo1, myLogo);
         rideresultlist.add(resultsetting);
-          resultsetting = new Resultsetting("Suhail T S", "Trivandrum", "Mumbai", " 10:50am", "  09:00pm", " 9Hrs", "Merceedes bens", "Kl1056", (float) 5, (float) 5, myLogo1, myLogo);
+        resultsetting = new Resultsetting("Suhail T S", "Trivandrum", "Mumbai", " 10:50am", "  09:00pm", " 9Hrs", "Merceedes bens", "Kl1056", (float) 3, (float) 3, myLogo1, myLogo);
 
 
         rideresultlist.add(resultsetting);
